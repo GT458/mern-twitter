be5ccbe887d2609b648f183cc0f504323465b0ce
GT458 <isohrob1@gmail.com>
2021-07-17 23:47:54 -0400

    adde users and tweets routes

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const express = require("express");
+const path = require('path');
+const users = require(path.resolve(__dirname, './routes/api/users'));
+const tweets = require(path.resolve(__dirname, './routes/api/tweets'));
+
+
 const app = express();
 // const test = require('./test')
-const path = require('path');
 const db = require(path.resolve(__dirname, './config/key.js')).mongoURI;
 
 mongoose
@@ -10,5 +14,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is runnign on port ${port}`));
